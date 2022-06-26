cbac0683f156faef54445361714d349844860c8a
pranaavr <68622267+pranaavr@users.noreply.github.com>
2022-06-25 20:49:33 -0400

    Time: 58 ms (29.12%), Space: 14.2 MB (26.85%) - LeetHub

---
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
+        words = text.split(' ')
+        broken = list(brokenLetters)
+        
+        for l in broken:
+            words = [x for x in words if l not in x]
+            print(l)
+            print(words)
+        
+        return len(words)
+        
